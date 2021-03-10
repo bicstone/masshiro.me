6345efd6cb42ab95e6e32427ff98e4c5487e2181
Takanori Oishi <takanori0014@live.jp>
2021-03-11 01:33:11 +0900

    chore: 2021年2月リリース (#206)

--- src/components/sections/Hello.tsx
+++ src/components/sections/Hello.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { useI18next } from 'gatsby-plugin-react-i18next';
-import { Typography, Grid, Button, makeStyles } from '@material-ui/core';
+import { Typography, Grid, Button, makeStyles, Paper, CardHeader } from '@material-ui/core';
 import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
 import { BicstoneIcon } from '../';
 import { useBreakPoint } from '../../hooks';
@@ -10,7 +10,9 @@ import { HelloDataQuery } from '../../types';
 const useStyles = makeStyles(theme => ({
   button: {
     marginRight: theme.spacing(1),
-    marginBottom: theme.spacing(1),
+  },
+  icon: {
+    textAlign: 'center',
   },
 }));
 
@@ -37,37 +39,45 @@ export const Hello: React.FC = () => {
   );
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Grid container spacing={2} justify="center" alignItems="center">
-          <Grid item>
-            <BicstoneIcon width={100} height={100} />
-          </Grid>
-          <Grid item xs={11} sm={10}>
-            <Typography component="span" variant="h6">
-              {t('hello.title')}
-            </Typography>
-            <Typography variant="body1" paragraph>
-              {t('hello.description')}
-            </Typography>
-            {allContentfulHello.edges.map(
-              ({ node }) =>
-                node.node_locale === language && (
-                  <Button
-                    href={node.href || ''}
-                    variant="outlined"
-                    color="secondary"
-                    size={BUTTON_SMALL_WIDTH.includes(width) ? 'small' : 'medium'}
-                    rel="external noreferrer noopener nofollow"
-                    className={classes.button}
-                    key={node.id}
-                  >
-                    {node.name}
-                  </Button>
-                ),
-            )}
-          </Grid>
-        </Grid>
+    <Grid container spacing={2} justify="center" alignItems="center">
+      <Grid item xs={12} sm={3} md={2} className={classes.icon}>
+        <BicstoneIcon width={100} height={100} />
+      </Grid>
+      <Grid item xs={12} sm={9} md={10}>
+        <Paper>
+          <CardHeader
+            disableTypography
+            title={
+              <Typography component="p" variant="h6" gutterBottom>
+                {t('hello.title')}
+              </Typography>
+            }
+            subheader={
+              <>
+                <Typography variant="body1" color="textPrimary" paragraph>
+                  {t('hello.description')}
+                </Typography>
+
+                {allContentfulHello.edges.map(
+                  ({ node }) =>
+                    node.node_locale === language && (
+                      <Button
+                        href={node.href || ''}
+                        variant="outlined"
+                        color="secondary"
+                        size={BUTTON_SMALL_WIDTH.includes(width) ? 'small' : 'medium'}
+                        rel="external noreferrer noopener nofollow"
+                        className={classes.button}
+                        key={node.id}
+                      >
+                        {node.name}
+                      </Button>
+                    ),
+                )}
+              </>
+            }
+          />
+        </Paper>
       </Grid>
     </Grid>
   );
